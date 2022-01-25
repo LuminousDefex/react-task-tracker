7aa31a1f82980160b79624a5ab16b5d8f2f0fad7
LuminousDefex <defex84@gmail.com>
2022-01-25 14:05:19 +0200

    added basic routing

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+        <div>
+            <header className="header">
+                <h1>Task Tracker</h1>
+            </header>
+            <h4>Version 1.0.0</h4>
+            <a href="/">Go back</a>
+        </div>
+    )
+};
+
+export default About;
